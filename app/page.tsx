371aadd7bd200f102f8aeebf3358610cd73300d3
yhwa <yhwabillie93@gmail.com>
2024-07-14 03:14:22 +0900

    배포 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>Hello EC2 Hello Docker Hello Github Actions! 240713</p>
+        <p>Hello EC2 Hello Docker Hello Github Actions! 20713</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
